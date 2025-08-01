6b5051146cceb355dd92adeb0d0318c58a437f3d
daxida <galmaw@hotmail.com>
2025-08-01 18:59:01 +0200

    Formatting

--- scripts.js
+++ scripts.js
@@ -162,7 +162,7 @@ function renderPlotCard1(acpCSVPath) {
 
       results.data.forEach(row => {
         xValues.push(parseFloat(row.PC1));
-        yValues.push(parseFloat(row.PC2)); 
+        yValues.push(parseFloat(row.PC2));
         pointData.push(row);
       });
 
@@ -206,7 +206,7 @@ function renderPlotCard1(acpCSVPath) {
   });
 }
 
-function renderPlotCard1_max(acpCSVPath) {
+function renderPlotCard1Max(acpCSVPath) {
   Papa.parse(acpCSVPath, {
     download: true,
     header: true,
@@ -221,7 +221,7 @@ function renderPlotCard1_max(acpCSVPath) {
         [0.333, "white"],
         [0.666, "red"],
         [1.0, "black"]
-      ]; 
+      ];
 
       results.data.forEach(row => {
         xValues.push(parseFloat(row.size));
@@ -231,93 +231,86 @@ function renderPlotCard1_max(acpCSVPath) {
 
         zValues.push(logZ);
         pointData.push(row);
-      });  
+      });
 
-            
       const hoverLabels = pointData.map(row =>
         `size: ${row.size}<br>gap: ${row.gap}<br>value: ${row.z}<br>ID: ${row.ID}`
       );
-      var trace1 = { 
-        x: xValues, 
-        y: yValues, 
-        mode: 'markers', 
-        name: 'points', 
+      var trace1 = {
+        x: xValues,
+        y: yValues,
+        mode: 'markers',
+        name: 'points',
         text: hoverLabels,
-        marker: {  
-          size: 20,  
+        marker: {
+          size: 20,
           cmin: -1,
           cmax: 2,
           color: zValues,
           colorscale: colorScale,
-        }, 
-        type: 'scatter' 
-      }; 
+        },
+        type: 'scatter'
+      };
 
-      var trace2 = { 
-        x: xValues, 
-        y: yValues, 
-        name: 'density', 
-        ncontours: 20, 
-        colorscale: "Greys", 
-        showscale: false, 
-        type: 'histogram2dcontour' 
+      var trace2 = {
+        x: xValues,
+        y: yValues,
+        name: 'density',
+        ncontours: 20,
+        colorscale: "Greys",
+        showscale: false,
+        type: 'histogram2dcontour'
       };
 
       var trace3 = {
-
-        x: xValues, 
-        name: 'x density', 
-        marker: {color: 'rgba(4, 92, 48, 1)'}, 
-        yaxis: 'y2', 
-        type: 'histogram' 
+        x: xValues,
+        name: 'x density',
+        marker: { color: 'rgba(4, 92, 48, 1)' },
+        yaxis: 'y2',
+        type: 'histogram'
       };
 
-      var trace4 = { 
-        y: yValues, 
-        name: 'y density', 
-        marker: {color: 'rgba(6, 81, 131, 1)'}, 
-        xaxis: 'x2', 
-        type: 'histogram' 
+      var trace4 = {
+        y: yValues,
+        name: 'y density',
+        marker: { color: 'rgba(6, 81, 131, 1)' },
+        xaxis: 'x2',
+        type: 'histogram'
       };
 
-      var data = [trace2,trace1, trace3, trace4];
+      var data = [trace2, trace1, trace3, trace4];
 
-      var layout = { 
+      var layout = {
         xaxis: { range: [3, 20], title: 'Arm Length' },
         yaxis: { range: [0, 20], title: 'Gap Length' },
-        showlegend: false, 
-        autosize: true,  
-        margin: {t: 50}, 
-        hovermode: 'closest', 
-        bargap: 0, 
-        xaxis: { 
-          domain: [0, 0.85], 
-          showgrid: false, 
-          zeroline: false 
+        showlegend: false,
+        autosize: true,
+        margin: { t: 50 },
+        hovermode: 'closest',
+        bargap: 0,
+        xaxis: {
+          domain: [0, 0.85],
+          showgrid: false,
+          zeroline: false
         },
-
-        yaxis: { 
-          domain: [0, 0.85], 
-          showgrid: false, 
-          zeroline: false 
+        yaxis: {
+          domain: [0, 0.85],
+          showgrid: false,
+          zeroline: false
         },
-
-        xaxis2: { 
-          domain: [0.85, 1], 
-          showgrid: false, 
-          zeroline: false 
+        xaxis2: {
+          domain: [0.85, 1],
+          showgrid: false,
+          zeroline: false
         },
-
-        yaxis2: { 
-          domain: [0.85, 1], 
-          showgrid: false, 
-          zeroline: false 
+        yaxis2: {
+          domain: [0.85, 1],
+          showgrid: false,
+          zeroline: false
         }
-
       };
 
       Plotly.newPlot('plot1', data, layout);
-      
 
       // Add click handler to card1 that updates plot3
       document.getElementById('plot1').on('plotly_click', function(eventData) {
@@ -382,7 +375,7 @@ function renderPlotsCard2(acpCSVPathPlot2, plotName, titleText) {
 // Top-left
 function buildPlotCard1() {
   // console.log("[BPC1] called buildPlotCard1");
-  const { tabLeftValue, tabRightValue, categoryValue, filterValue,graphValue } = getCurrentSelections();
+  const { tabLeftValue, tabRightValue, categoryValue, filterValue, graphValue } = getCurrentSelections();
 
   findFilePathFromJSON(filterValue).then(pathArray => {
     if (!pathArray) {
@@ -393,20 +386,21 @@ function buildPlotCard1() {
     const folderPath = pathArray.join("/"); // This is not the whole path!
     // console.log(`folderPath=${folderPath} found with filterValue=${filterValue}.`);
 
-    const rootPath = `${DATA_DIR}/philogenie/${folderPath}`; 
-
-    const parameters = [tabRightValue,tabLeftValue,filterValue]
+    const rootPath = `${DATA_DIR}/philogenie/${folderPath}`;
+    const parameters = [tabRightValue, tabLeftValue, filterValue]
       .map(String)
       .join("_");
+    const acpCSVPath = `${rootPath}/${graphValue}_${parameters}.csv`;
 
-    const acpCSVPath = `${rootPath}/${graphValue}_${parameters}.csv`; 
+    switch (graphValue) {
+      case "acp":
+        renderPlotCard1(acpCSVPath);
+        break;
+      case "max":
+        renderPlotCard1Max(acpCSVPath);
+        break;
+    }
 
-    if (graphValue === "acp") {
-      renderPlotCard1(acpCSVPath);
-    }
-    if (graphValue === "max") {
-      renderPlotCard1_max(acpCSVPath);
-    }
     const acp1CSVPath = `${rootPath}/PC0_${parameters}.csv`
     const acp2CSVPath = `${rootPath}/PC1_${parameters}.csv`
 
